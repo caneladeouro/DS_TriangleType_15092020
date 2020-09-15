1171f29d6769923a78d0467c3e08a6fbc530dced
Matheus dos Santos Bonifacio <caneladeouro10@gmail.com>
2020-09-15 16:14:38 -0300

    Score_15092020_16H14

---
+++ DS_Triangulo_15092020_15H37/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace DS_Triangulo_15092020_15H37
+{
+    partial class fmrCalculate
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSide1 = new System.Windows.Forms.TextBox();
+            this.lblSideOne = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSide2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSide3 = new System.Windows.Forms.TextBox();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtSide1
+            // 
+            this.txtSide1.Location = new System.Drawing.Point(76, 54);
+            this.txtSide1.Name = "txtSide1";
+            this.txtSide1.Size = new System.Drawing.Size(100, 20);
+            this.txtSide1.TabIndex = 0;
+            // 
+            // lblSideOne
+            // 
+            this.lblSideOne.AutoSize = true;
+            this.lblSideOne.Location = new System.Drawing.Point(12, 61);
+            this.lblSideOne.Name = "lblSideOne";
+            this.lblSideOne.Size = new System.Drawing.Size(40, 13);
+            this.lblSideOne.TabIndex = 1;
+            this.lblSideOne.Text = "Lado 1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Lado 2";
+            // 
+            // txtSide2
+            // 
+            this.txtSide2.Location = new System.Drawing.Point(76, 81);
+            this.txtSide2.Name = "txtSide2";
+            this.txtSide2.Size = new System.Drawing.Size(100, 20);
+            this.txtSide2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Lado 3";
+            // 
+            // txtSide3
+            // 
+            this.txtSide3.Location = new System.Drawing.Point(76, 106);
+            this.txtSide3.Name = "txtSide3";
+            this.txtSide3.Size = new System.Drawing.Size(100, 20);
+            this.txtSide3.TabIndex = 4;
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(15, 161);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(75, 23);
+            this.btnCheck.TabIndex = 6;
+            this.btnCheck.Text = "Verificar";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // fmrCalculate
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSide3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSide2);
+            this.Controls.Add(this.lblSideOne);
+            this.Controls.Add(this.txtSide1);
+            this.Name = "fmrCalculate";
+            this.Text = "Calcular Triangulo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSide1;
+        private System.Windows.Forms.Label lblSideOne;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSide2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSide3;
+        private System.Windows.Forms.Button btnCheck;
+    }
+}
+
---
+++ DS_Triangulo_15092020_15H37/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DS_Triangulo_15092020_15H37
+{
+    public partial class fmrCalculate : Form
+    {
+        public fmrCalculate()
+        {
+            InitializeComponent();
+        }
+
+        public class Triangulo
+        {
+            public double hipotenusa;
+            public double[] cateto = new double[2];
+            public double quadradoCateto;
+
+            public void aplpyValue(string a, string b, string c)
+            {
+                this.cateto[0] = double.Parse(a); this.cateto[1] = double.Parse(b);
+                this.hipotenusa = double.Parse(c);
+            }
+
+            public bool testTriangulo()
+            {
+                double[] add = new double[3];
+                add[0] = this.cateto[0] + this.cateto[1];
+                add[1] = this.cateto[1] + this.hipotenusa;
+                add[2] = this.hipotenusa + this.cateto[0];
+                if (add[0] > this.hipotenusa && add[1] > this.cateto[0] && add[2] > this.cateto[1]) { return true; } else { return false; }
+            }
+
+            public int TriangleType()
+            {
+                if (this.cateto[0] == this.cateto[1] && this.cateto[1] == this.hipotenusa && this.hipotenusa == this.cateto[0]) return 0;
+                else if (this.cateto[0] == this.cateto[1] || this.cateto[1] == this.hipotenusa || this.hipotenusa == this.cateto[0]) return 1;
+                else return 2;
+            }
+
+            public bool equalityTest()
+            {
+                double QuadradoCateto = Math.Sqrt(Math.Pow(this.cateto[0], 2) + Math.Pow(this.cateto[1], 2));
+                if (QuadradoCateto == this.hipotenusa) { this.quadradoCateto = QuadradoCateto; return true; } else { return false; };
+            }
+
+            public bool numberBigger()
+            {
+                if (this.quadradoCateto > this.hipotenusa) { return true; } else { return false; }
+            }
+        }
+
+        public double Call(string side)
+        {
+
+            double result = double.Parse(side);
+
+            return result;
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            int typeTriangle;
+            Triangulo triangulo = new Triangulo();
+            triangulo.aplpyValue(txtSide1.Text, txtSide2.Text, txtSide3.Text); typeTriangle = triangulo.TriangleType();
+
+            if (triangulo.testTriangulo())
+            {
+                switch (typeTriangle)
+                {
+                    case 0:
+                        MessageBox.Show("É um triangulo equilátero, e actângulo");
+                        break;
+                    case 1:
+                        MessageBox.Show("É um triangulo isósceles");
+                        break;
+                    default:
+                        MessageBox.Show("É um triangulo escaleno");
+                        break;
+                }
+            }
+            else MessageBox.Show("Isso não é um Triangulo");
+        }
+    }
+}
